d9e34080fa2bd3c74b43c8dd3a1c408115cd5e6f
GunaSekharRavupalli <111333435+GunaSekharRavupalli@users.noreply.github.com>
2023-01-31 15:14:46 +0530

    Add files via upload

---
+++ mp.py
@@ -0,0 +1,7 @@
+apple=100
+pineapple=200
+banana=300
+print(apple,end='*')
+print(pineapple,end='-')
+print(banana,end='/')
+print(apple,banana,pineapple)
---
+++ split.py
@@ -0,0 +1,8 @@
+text = 'geeks@for@geeks'
+print(text.split('@'))
+word = 'geeks3for33geeks'
+print(word.split('3',2))
+word = 'geeks:for:geeks'
+print(word.split(':'))
+word = 'CatBatSatFatOr'
+print(word.split('t'))
